bf09dc48d5965cfed382f97dc60cd542159babeb
KevinDGnanih <kevin.gnanih@gmail.com>
2022-04-10 22:02:56 +0000

    Add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenave initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenave initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmm, yyyy",
+    i18n: {done: "select"}
   });
+
+  // select initialization
+  document.addEventListener('DOMContentLoaded', function() {
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects)
+  });
+
+});
